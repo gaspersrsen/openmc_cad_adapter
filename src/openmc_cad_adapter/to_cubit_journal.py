c00ac1fe1f55087ccd9c44814a50644ca6920406
gaspersrsen <gasper.srsen@hotmail.com>
2025-03-03 15:25:06 +0100

    a

--- src/openmc_cad_adapter/to_cubit_journal.py
+++ src/openmc_cad_adapter/to_cubit_journal.py
@@ -113,7 +113,7 @@ def to_cubit_journal(geometry : openmc.Geometry, world : Iterable[Real] = None,
         #s = surface_to_cubit_journal( node.region, w)
         strt = body_next()
         all_ids = np.append(np.array(ids), np.array(s))
-        print(all_ids)
+        print(f"{{ {' '.join( map(str, np.array(all_ids)) )} }}")
         
         #exec_cubit( f"intersect body {{ {ids} }} {{ {s} }} keep" )
         #exec_cubit( f"intersect body {{ {' '.join( map(str, np.array(all_ids)) )} }} keep" )
