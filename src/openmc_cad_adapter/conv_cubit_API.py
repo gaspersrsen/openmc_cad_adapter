b15eb7aa7edb594d159cf8d397d2547180de7c24
gaspersrsen <gasper.srsen@hotmail.com>
2025-03-03 13:51:36 +0100

    a

--- src/openmc_cad_adapter/conv_cubit_API.py
+++ src/openmc_cad_adapter/conv_cubit_API.py
@@ -1,19 +1,17 @@
 import sys
 sys.path.append('/opt/cubit/bin')
 sys.path.append('/opt/Coreform-Cubit-2025.1/bin/')
-# import cubit
+import cubit
 
-# #cubit.init([])
-# cubit.reset()
+#cubit.init([])
+cubit.reset()
 
 def exec_cubit(command):
-    print(command)
-    #return cubit.cmd(command)
+    #print(command)
+    return cubit.cmd(command)
 
 def body_id():
-    return 1
-    #cubit.get_last_id("body")
+    return cubit.get_last_id("body")
 
 def body_next():
-    return 2
-    #cubit.get_next_block_id()
+    return cubit.get_next_block_id()
