a707a4c727895ac4bd3362eba752eeac40adcee5
gaspersrsen <gasper.srsen@hotmail.com>
2025-03-04 12:23:14 +0100

    a

--- src/openmc_cad_adapter/to_cubit_journal.py
+++ src/openmc_cad_adapter/to_cubit_journal.py
@@ -303,11 +303,12 @@ def to_cubit_journal(geometry : openmc.Geometry, world : Iterable[Real] = None,
     # Process geometry
     final_ids = process_node(geom.root_universe, w)
     
-    # Cleanup
-    # exec_cubit(f"brick x {world[0]} y {world[1]} z {world[2]}\n")
-    # for i in range(1,body_id()+1,1):
-    #     if i not in final_ids:
-    #         exec_cubit( f"delete volume {{ {i} }}" )
+    Cleanup
+    exec_cubit(f"brick x {world[0]} y {world[1]} z {world[2]}\n")
+    for i in range(1,body_id()+1,1):
+        if i not in final_ids:
+            if i == 16 or i==327: pass
+            exec_cubit( f"delete volume {{ {i} }}" )
     
     #Finalize
     exec_cubit("graphics flush\n")
