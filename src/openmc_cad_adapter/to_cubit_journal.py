40f1536cbc6299384ad4b5bdeb9080282564e1fc
gaspersrsen <gasper.srsen@hotmail.com>
2025-03-07 11:51:08 +0100

    a

--- src/openmc_cad_adapter/to_cubit_journal.py
+++ src/openmc_cad_adapter/to_cubit_journal.py
@@ -307,7 +307,7 @@ def to_cubit_journal(geometry : openmc.Geometry,
                     cell_mat[ids3[a]] = cell_mat[ids[a]]
                 except:
                     pass
-            exec_cubit( f"volume {to_cubit_list(ids3)} move {to_cubit_list(midp(node.bounding_box))}" )
+            exec_cubit( f"volume {to_cubit_list(ids3)} move {to_cubit_list(-1*midp(node.bounding_box))}" )
             ids_out = trim_uni(node, ids3, bb)
             return ids_out
         
