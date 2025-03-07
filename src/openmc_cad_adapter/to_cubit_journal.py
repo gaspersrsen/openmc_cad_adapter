de525a5e24e02c698728da2debd15d86c3e0a249
gaspersrsen <gasper.srsen@hotmail.com>
2025-03-07 16:34:37 +0100

    a

--- src/openmc_cad_adapter/to_cubit_journal.py
+++ src/openmc_cad_adapter/to_cubit_journal.py
@@ -383,14 +383,15 @@ def to_cubit_journal(geometry : openmc.Geometry,
             exec_cubit( f'Block {mat_map[mat_n]} add volume {id}' )
         
     def p_m(ids):
-        all_mat = set(mat_map.values())
-        for mat in all_mat:
-            print(mat)
-            # mat_ids = [k for k,v in mat_map.items() if v==mat.name and k in ids]
-            # exec_cubit( f'create material name "{mat.name}" ' )
-            # b_id = block_next()
-            # exec_cubit( f'Block {b_id} add volume {mat_ids}' )
-            # exec_cubit( f'Block {b_id} material "{mat.name}"' )
+        print(mat_map.values())
+        # all_mat = set(mat_map.values())
+        # for mat in all_mat:
+        #     print(mat)
+        #     # mat_ids = [k for k,v in mat_map.items() if v==mat.name and k in ids]
+        #     # exec_cubit( f'create material name "{mat.name}" ' )
+        #     # b_id = block_next()
+        #     # exec_cubit( f'Block {b_id} add volume {mat_ids}' )
+        #     # exec_cubit( f'Block {b_id} material "{mat.name}"' )
             
     # Initialize commands
     # exec_cubit("set echo off\n")
