0140a4de57f07f532455269fcd12362866cb03c5
gaspersrsen <gasper.srsen@hotmail.com>
2025-05-16 21:29:15 +0200

    a

--- src/openmc_cad_adapter/to_cubit_journal.py
+++ src/openmc_cad_adapter/to_cubit_journal.py
@@ -395,6 +395,7 @@ def to_cubit_journal(geometry : openmc.Geometry,
                     cell_mat[ids5[a]] = outer_cell_mat[0]
                 ids = np.append(ids, np.array(ids5)).astype(int)
                 latt_map[node.id] = ids
+                print(latt_map[node.id])
             return latt_map[node.id]
     
     def propagate_mat(id):
