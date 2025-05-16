f8870906394431ff99b38698f18d7401411371c9
gaspersrsen <gasper.srsen@hotmail.com>
2025-05-16 12:27:17 +0200

    a

--- src/openmc_cad_adapter/to_cubit_journal.py
+++ src/openmc_cad_adapter/to_cubit_journal.py
@@ -345,9 +345,9 @@ def to_cubit_journal(geometry : openmc.Geometry,
                                 strt = last_id(volume_id()) + 1
                                 exec_cubit( f" volume {to_cubit_list(ids2)} copy" )
                                 stp = last_id(volume_id())
-                                if strt == stp+1:
-                                    ValueError(f"No volume copied, {cell} is empty, type:{type(cell)},cell:{str(cell)}")
                                 ids3 = range(strt,stp+1,1)
+                                if len(np.array(ids3))==0:
+                                    ValueError(f"No volume copied, {cell} is empty, type:{type(cell)},cell:{str(cell)}")
                                 for a in range(len(ids3)):
                                     cell_mat[ids3[a]] = cell_mat[ids2[a]]
                                 ids4 = trim_cell_like(ids3, base_rect)
