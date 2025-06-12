8e3e4c71ad224ef173bdda5779725a9104f6bd8a
gaspersrsen <gasper.srsen@hotmail.com>
2025-06-12 16:50:34 +0200

    a

--- src/openmc_cad_adapter/to_cubit_journal.py
+++ src/openmc_cad_adapter/to_cubit_journal.py
@@ -293,15 +293,16 @@ def to_cubit_journal(geometry : openmc.Geometry,
                     ids = np.append(ids,np.array(process_node( c, w, midp(node.bounding_box)-center_world))).astype(int)
                 uni_map[node.id] = ids
             ids = uni_map[node.id]
-            exec_cubit(f"brick x {world[0]} y {world[1]} z {world[2]}\n")
-            strt = volume_id() + 1
+            # exec_cubit(f"brick x {world[0]} y {world[1]} z {world[2]}\n")
+            # strt = volume_id() + 1
             exec_cubit( f" volume { to_cubit_list(ids) } copy" )
-            stp = last_id(volume_id())
-            ids3 = range(strt,stp+1,1)
+            #stp = last_id(volume_id())
+            ids3 = np.array(volume_id()).flatten()#range(strt,stp+1,1)
             for a in range(len(ids3)):
                 try:
                     cell_mat[ids3[a]] = cell_mat[ids[a]]
                 except:
+                    print(f"Material of cell {ids[a]} is empty")
                     pass
             move_vec = center_world-midp(node.bounding_box)
             if any(move_vec != 0):
