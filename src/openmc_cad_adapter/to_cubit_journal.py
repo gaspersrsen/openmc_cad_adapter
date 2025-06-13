f00cd342c2f8c4f292a73ec5091d6d5ec668dc2e
gaspersrsen <gasper.srsen@hotmail.com>
2025-06-13 14:15:01 +0200

    a

--- src/openmc_cad_adapter/to_cubit_journal.py
+++ src/openmc_cad_adapter/to_cubit_journal.py
@@ -385,7 +385,8 @@ def to_cubit_journal(geometry : openmc.Geometry,
                                     ids3 = np.array([volume_id()]).flatten()
                                     for a in range(len(ids3)):
                                         cell_mat[ids3[a]] = cell_mat[ids2[a]]
-                                    print(cell, cell.id)
+                                    print(cell, cell.id, no_trim)
+                                    print(cell not in no_trim)
                                     if cell not in no_trim:
                                         ids3 = trim_cell_like(ids3, base_rect)
                                     latt_map_trim[f"{node.id}_{cell.id}"] = ids3
